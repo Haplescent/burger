e5dd6493be1dcf3952361c514dabf0e898b66a97
John Merritt <johnmerritt95@gmail.com>
2020-07-05 14:24:25 -0700

    restructuring my state to pass props without refreshing screen

--- views/index.js
+++ views/index.js
@@ -1,59 +1,6 @@
-const DevourButton = (props) => {
-  const id = props.id;
-  const [devoured, setDevour] = React.useState(0);
-
-  const handleDevour = async (DevourId) => {
-    console.log(`you hit like ${DevourId}`);
-    await axios
-      .post("/api/update", {
-        devoured: true,
-        id: DevourId + 1,
-      })
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-    this.setDevour(true);
-  };
-
-  React.useEffect(() => {
-    // Update the document title using the browser API
-    axios
-      .get("/api/all")
-      .then((response) => {
-        // handle success
-        setDevour(response.data[id].devoured);
-      })
-      .catch((error) => console.log(error));
-  }, devoured);
-
-  if (devoured) {
-    return <p>You devoured this.</p>;
-  } else {
-    return <button onClick={() => handleDevour(id)}>Devour</button>;
-  }
-};
-
-const BurgerList = () => {
-  const [burgerData, setBurgerData] = React.useState([]);
-  const listItems = burgerData.map((burger) => (
-    <li>
-      <DevourButton id={parseInt(burger.id) - 1} />
-      {burger.burger_name}
-    </li>
-  ));
-
-  React.useEffect(() => {
-    axios
-      .get("/api/all")
-      .then((response) => {
-        setBurgerData(response.data);
-      })
-      .catch((error) => console.log(error));
-  }, burgerData);
-
+const BurgerList = (props) => {
+  const burgerComponentArray = props.burgerComponentArray;
+  const listItems = burgerComponentArray.map((burger) => <li>{burger}</li>);
   return (
     <div>
       <ul>{listItems}</ul>
@@ -61,19 +8,15 @@ const BurgerList = () => {
   );
 };
 
-const BurgerForm = () => {
-  const [value, setValue] = React.useState("");
+const App = () => {
+  const [burgerData, setBurgerData] = React.useState([]);
 
-  const handleChange = (event) => {
-    setValue(event.target.value);
-  };
-
-  const handleSubmit = async (event) => {
-    alert("A name was submitted: " + value);
-    event.preventDefault();
+  const handleDevour = async (DevourId) => {
+    console.log(`you hit like ${DevourId}`);
     await axios
-      .post("/api/add", {
-        burgerName: value,
+      .post("/api/update", {
+        devoured: 1,
+        id: DevourId,
       })
       .then(function (response) {
         console.log(response);
@@ -81,27 +24,152 @@ const BurgerForm = () => {
       .catch(function (error) {
         console.log(error);
       });
-    setValue("");
+
+    const ArrayIndex = parseInt(DevourId) - 1;
+    console.log(burgerData);
+    console.log(ArrayIndex);
+    burgerData[ArrayIndex].devoured = 1;
+    setBurgerData(burgerData);
   };
 
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={value} onChange={handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-      <BurgerList />
-    </div>
-  );
+  const DevourButton = (props) => {
+    const id = props.burger.id;
+    const devoured = props.burger.devoured;
+    const burgerName = props.burger.burger_name;
+
+    if (devoured) {
+      return <p>You devoured this.</p>;
+    } else {
+      return (
+        <button onClick={() => props.handleClick(id)}>
+          Devour {burgerName}
+        </button>
+      );
+    }
+  };
+
+  React.useEffect(() => {
+    axios
+      .get("/api/all")
+      .then((response) => {
+        setBurgerData(response.data);
+        console.log("react useEffect ran");
+        console.log(response.data);
+      })
+      .catch((error) => console.log(error));
+  }, burgerData);
+
+  const burgerArray = burgerData.map((burger) => (
+    <DevourButton burger={burger} handleClick={handleDevour} />
+  ));
+
+  return <BurgerList burgerComponentArray={burgerArray} />;
 };
+
+// const DevourButton = (props) => {
+//   const id = props.id;
+//   const [devoured, setDevour] = React.useState(0);
+
+//   const handleDevour = async (DevourId) => {
+//     console.log(`you hit like ${DevourId}`);
+//     await axios
+//       .post("/api/update", {
+//         devoured: true,
+//         id: DevourId + 1,
+//       })
+//       .then(function (response) {
+//         console.log(response);
+//       })
+//       .catch(function (error) {
+//         console.log(error);
+//       });
+//     this.setDevour(true);
+//   };
+
+//   React.useEffect(() => {
+//     // Update the document title using the browser API
+//     axios
+//       .get("/api/all")
+//       .then((response) => {
+//         // handle success
+//         setDevour(response.data[id].devoured);
+//       })
+//       .catch((error) => console.log(error));
+//   }, devoured);
+
+//   if (devoured) {
+//     return <p>You devoured this.</p>;
+//   } else {
+//     return <button onClick={() => handleDevour(id)}>Devour</button>;
+//   }
+// };
+
+// const BurgerList = () => {
+//   const [burgerData, setBurgerData] = React.useState([]);
+//   const listItems = burgerData.map((burger) => (
+//     <li>
+//       <DevourButton id={parseInt(burger.id) - 1} />
+//       {burger.burger_name}
+//     </li>
+//   ));
+
+//   React.useEffect(() => {
+//     axios
+//       .get("/api/all")
+//       .then((response) => {
+//         setBurgerData(response.data);
+//       })
+//       .catch((error) => console.log(error));
+//   }, burgerData);
+
+//   return (
+//     <div>
+//       <ul>{listItems}</ul>
+//     </div>
+//   );
+// };
+
+// const BurgerForm = () => {
+//   const [value, setValue] = React.useState("");
+
+//   const handleChange = (event) => {
+//     setValue(event.target.value);
+//   };
+
+//   const handleSubmit = async (event) => {
+//     alert("A name was submitted: " + value);
+//     event.preventDefault();
+//     await axios
+//       .post("/api/add", {
+//         burgerName: value,
+//       })
+//       .then(function (response) {
+//         console.log(response);
+//       })
+//       .catch(function (error) {
+//         console.log(error);
+//       });
+//     setValue("");
+//   };
+
+//   return (
+//     <div>
+//       <form onSubmit={handleSubmit}>
+//         <label>
+//           Name:
+//           <input type="text" value={value} onChange={handleChange} />
+//         </label>
+//         <input type="submit" value="Submit" />
+//       </form>
+//       <BurgerList />
+//     </div>
+//   );
+// };
 
 const domContainer = document.querySelector("#like_button_container");
 ReactDOM.render(
   <div>
-    <BurgerForm />
+    <App />
   </div>,
   domContainer
 );
