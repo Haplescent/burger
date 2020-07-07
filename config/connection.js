70170fec72d426a5e391c415df5307e826ce7252
John Merritt <johnmerritt95@gmail.com>
2020-07-06 17:27:38 -0700

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@ const mysql = require("mysql");
 require("dotenv").config();
 
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: "umabrisfx8afs3ja.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
   port: 3306,
-  user: "root",
+  user: "k7xp3ohfhasyaw7z",
   password: process.env.PASSWORD,
-  database: "burgers_db",
+  database: "grcbjo3rydlpiea1",
 });
 
 module.exports = connection;
